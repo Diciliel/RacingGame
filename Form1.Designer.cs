c0473665d7fd514f32a6b99cf0445ae4267d6670
Diciliel <diclesoylu@gmail.com>
2023-07-31 11:59:33 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace RacingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.explosion = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.picBox_car2 = new System.Windows.Forms.PictureBox();
+            this.picBox_car1 = new System.Windows.Forms.PictureBox();
+            this.picBox_road = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_score = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_HS = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_road)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.explosion);
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Controls.Add(this.picBox_car2);
+            this.panel1.Controls.Add(this.picBox_car1);
+            this.panel1.Controls.Add(this.picBox_road);
+            this.panel1.Location = new System.Drawing.Point(18, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(507, 523);
+            this.panel1.TabIndex = 0;
+            // 
+            // explosion
+            // 
+            this.explosion.Image = global::RacingGame.Properties.Resources.explosion;
+            this.explosion.Location = new System.Drawing.Point(231, 314);
+            this.explosion.Name = "explosion";
+            this.explosion.Size = new System.Drawing.Size(48, 52);
+            this.explosion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.explosion.TabIndex = 2;
+            this.explosion.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::RacingGame.Properties.Resources.araba5;
+            this.player.Location = new System.Drawing.Point(231, 361);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(58, 117);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // picBox_car2
+            // 
+            this.picBox_car2.Image = global::RacingGame.Properties.Resources.araba8;
+            this.picBox_car2.Location = new System.Drawing.Point(359, 72);
+            this.picBox_car2.Name = "picBox_car2";
+            this.picBox_car2.Size = new System.Drawing.Size(80, 121);
+            this.picBox_car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBox_car2.TabIndex = 1;
+            this.picBox_car2.TabStop = false;
+            // 
+            // picBox_car1
+            // 
+            this.picBox_car1.Image = global::RacingGame.Properties.Resources.araba4;
+            this.picBox_car1.Location = new System.Drawing.Point(101, 72);
+            this.picBox_car1.Name = "picBox_car1";
+            this.picBox_car1.Size = new System.Drawing.Size(60, 127);
+            this.picBox_car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBox_car1.TabIndex = 1;
+            this.picBox_car1.TabStop = false;
+            // 
+            // picBox_road
+            // 
+            this.picBox_road.Image = global::RacingGame.Properties.Resources.yol;
+            this.picBox_road.Location = new System.Drawing.Point(-3, -251);
+            this.picBox_road.Name = "picBox_road";
+            this.picBox_road.Size = new System.Drawing.Size(513, 775);
+            this.picBox_road.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox_road.TabIndex = 0;
+            this.picBox_road.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(34, 557);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            // 
+            // lbl_score
+            // 
+            this.lbl_score.AutoSize = true;
+            this.lbl_score.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_score.Location = new System.Drawing.Point(116, 557);
+            this.lbl_score.Name = "lbl_score";
+            this.lbl_score.Size = new System.Drawing.Size(21, 22);
+            this.lbl_score.TabIndex = 1;
+            this.lbl_score.Text = "0";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_start.Location = new System.Drawing.Point(216, 595);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(92, 38);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 16;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(351, 557);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "High Score:";
+            // 
+            // lbl_HS
+            // 
+            this.lbl_HS.AutoSize = true;
+            this.lbl_HS.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_HS.Location = new System.Drawing.Point(476, 557);
+            this.lbl_HS.Name = "lbl_HS";
+            this.lbl_HS.Size = new System.Drawing.Size(21, 22);
+            this.lbl_HS.TabIndex = 1;
+            this.lbl_HS.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 685);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.lbl_HS);
+            this.Controls.Add(this.lbl_score);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Racing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_road)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picBox_road;
+        private System.Windows.Forms.PictureBox picBox_car1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox picBox_car2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_score;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.PictureBox explosion;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_HS;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using RacingGame.Properties;
+
+namespace RacingGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int kazanilanPuan = 0;
+        int yolHizi = 5;
+        int PlayerHizi = 8; // Player
+
+        bool solYon = false;
+        bool sagYon = false;
+
+        int arabaHizi = 6; // Diger arabalar
+
+        Random rnd = new Random();
+
+        public void GameStart()
+        {
+            btn_start.Enabled = false; // Oyun baslayinca button pasifleşir, basilamaz.
+            explosion.Visible = false; // Patlama efekti surekli gorunmesin diye.
+
+            kazanilanPuan = 0; // Oyun her basladiginda score sifirlanir.
+            lbl_HS.Text = Settings2.Default.highScore.ToString();
+
+            player.Left = 160; // Araba soldan 160 px uzakta baslar.
+            player.Top = 350; 
+
+            picBox_car1.Left = 120;
+            picBox_car1.Top = 30;
+
+            picBox_car2.Left = 320;
+            picBox_car2.Top = 70;
+
+            //explosion.Left = 165;
+            //explosion.Top = 294;
+
+            solYon = false;
+            sagYon = false;
+
+
+            timer1.Start();
+            sesAc();
+        }
+
+        private void araba1Degis()
+        {
+            int sira = rnd.Next(1,7); // Resources klasorundeki gorsellerden random secilen gelir.
+            switch (sira)
+            {
+                case 1:
+                    picBox_car1.Image = Properties.Resources.araba2;
+                    arabaHizi = 8;
+                    break;
+                case 2:
+                    picBox_car1.Image = Properties.Resources.araba3;
+                    arabaHizi = 8;
+                    break;
+                case 3:
+                    picBox_car1.Image = Properties.Resources.araba6;
+                    break;
+                case 4:
+                    picBox_car1.Image = Properties.Resources.araba7;
+                    break;
+                case 5:
+                    picBox_car1.Image= Properties.Resources.araba8;
+                    break;
+                case 6:
+                    picBox_car1.Image= Properties.Resources.araba9;
+                    break;
+                default: // Default secili olan gelir.
+                    break;
+            }
+        }
+
+        private void araba2Degis()
+        {
+            int sira = rnd.Next(1, 7); // Resources klasorundeki gorsellerden random secilen gelir.
+            switch (sira)
+            {
+                case 1:
+                    picBox_car2.Image = Properties.Resources.araba2;
+                    arabaHizi= 8;
+                    break;
+                case 2:
+                    picBox_car2.Image = Properties.Resources.araba3;
+                    arabaHizi = 8;
+                    break;
+                case 3:
+                    picBox_car2.Image = Properties.Resources.araba4;
+                    break;
+                case 4:
+                    picBox_car2.Image = Properties.Resources.araba6;
+                    break;
+                case 5:
+                    picBox_car2.Image = Properties.Resources.araba7;
+                    break;
+                case 6:
+                    picBox_car1.Image = Properties.Resources.araba9;
+                    break;
+                default: // Default secili olan gelir.
+                    break;
+            }
+        }
+
+        private void GameOver()
+        { 
+            timer1.Stop();
+            
+            if (Convert.ToInt32(lbl_score.Text) > Convert.ToInt32(Settings2.Default.highScore.ToString()))
+            { 
+                Settings2.Default.highScore = lbl_score.Text;
+            }
+            
+            btn_start.Enabled = true;
+            explosion.Visible = true;
+            player.Controls.Add(explosion); // Carpma efekti arabayi takip eder, uzerinde belirir.
+            explosion.Location = new Point(7, -5);
+            explosion.BringToFront(); // Efekt gorseli onde kalacak
+            explosion.BackColor = Color.Transparent;
+            MessageBox.Show("Hepi Topu Bu Kadar Puan: " + lbl_score.Text, "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GameStart();
+        }
+        private void sesAc()
+        { 
+            SoundPlayer ses = new SoundPlayer();
+            string sesYol = Application.StartupPath + "\\race-track.wav";
+            ses.SoundLocation = sesYol;
+            ses.Play();
+        }
+
+        private void vurmaSesi() 
+        {
+            SoundPlayer vurma = new SoundPlayer();
+            string vurmasesi = Application.StartupPath + "\\hit.wav";
+            vurma.SoundLocation = vurmasesi;
+            vurma.Play();
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) // Klavyeye basildiginda calisan fonksiyon
+        {
+            if (e.KeyCode == Keys.Left && player.Left > 0)
+            {
+                solYon = true;
+            }
+            if (e.KeyCode == Keys.Right && player.Left + player.Width < panel1.Width)
+            {
+                sagYon = true;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e) // Klavyeye basmayi birakinca calisan fonksiyon
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                solYon = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                sagYon = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            kazanilanPuan++; // Oyun baslayinca puan surekli artar.
+            lbl_score.Text= kazanilanPuan.ToString();  // score textine yazar.
+
+            picBox_road.Top += yolHizi; // Yol yukarindan yol hizi (5px) kadar kayar.
+
+            if (picBox_road.Top > 200) // Yol görselinin yenilenmesi icin.
+            { 
+                picBox_road.Top = -105; 
+            }
+
+            if (solYon) // Araba, hizi kadar sag veya sola gidecek.
+            {
+                player.Left -= arabaHizi;
+            }
+            if (sagYon) 
+            {
+                player.Left += arabaHizi;
+            }
+
+            if (player.Left < 1) // Player oyun ekraninin disina cikamayacak.
+            {
+                solYon = false;
+            }
+            else if (player.Left + player.Width > 500)
+            {
+                sagYon = false;
+            }
+
+            picBox_car1.Top += arabaHizi; // Diger arabalar da hareket edecek.
+            picBox_car2.Top += arabaHizi;
+
+            if (picBox_car1.Top > panel1.Height) // Ilerledikce random yeni araba gelecek.
+            {
+                araba1Degis();
+                picBox_car1.Left = rnd.Next(50, 180);
+                picBox_car1.Top = rnd.Next(40, 80)* -1; // Yeni araba ekranin disindan belirecek.
+            }
+
+            if (picBox_car2.Top > panel1.Height) // Ilerledikce random yeni araba gelecek.
+            {
+                araba2Degis();
+                picBox_car2.Left = rnd.Next(200, 450);
+                picBox_car2.Top = rnd.Next(100,150) * -1; // Yeni araba ekranin disindan belirecek.
+            }
+
+            if (Convert.ToInt32(lbl_score.Text) == 500) 
+            {   
+                yolHizi = 8; 
+                arabaHizi = 11; 
+                PlayerHizi = 15; 
+            }
+
+            if (player.Bounds.IntersectsWith(picBox_car1.Bounds)|| player.Bounds.IntersectsWith(picBox_car2.Bounds)) // Arabalarin carpismasini kontrol eder. Ayni konumdalar mi?
+            {
+                vurmaSesi();
+                GameOver();             
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            GameStart();
+        }
+    }
+}
